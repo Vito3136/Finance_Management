0c39af733c224300efa06bebbf7cee1247625588
Vito3136 <vito3136@gmail.com>
2026-07-07 12:03:11 +0200

    Update Service Worker to Network First

--- sw.js
+++ sw.js
@@ -18,12 +18,20 @@ self.addEventListener('install', event => {
   );
 });
 
-// Fetch event for offline support
+// Fetch event (Network First, fallback to cache)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
+    fetch(event.request)
+      .then(networkResponse => {
+        // Se la rete funziona, aggiorniamo la cache con la nuova versione
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        // Se non c'è rete (offline), usiamo la cache
+        return caches.match(event.request);
       })
   );
 });
